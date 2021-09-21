dba6b500f47045e600f505cb0446b7b40471138a
AkshayAkkalagari <86343905+AkshayAkkalagari@users.noreply.github.com>
2021-09-21 08:44:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Route } from "react-router-dom";
 
 //HOC
 import DefaultHOC from "./HOC/default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
 //Pages
 import HomePage from "./Pages/Home.Page";
+import Movie from "./Pages/Movie.Page";
+import Plays from "./Pages/Plays.Pages";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -15,6 +18,8 @@ function App() {
   return (
     <>
      <DefaultHOC path="/" exact component={HomePage} />
+     <MovieHOC path="/Movie/:id" exact component={Movie} />
+     <DefaultHOC path="/plays" exact component={Plays} />
     </>
   );
 }
